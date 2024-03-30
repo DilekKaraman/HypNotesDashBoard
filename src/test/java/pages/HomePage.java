612efa12f60fee2be6ffa9e8d7597ea464cfdeb1
ZaferD <test01012018@gmail.com>
2024-03-30 15:24:51 +0100

    US_003 Scenario and Test Case

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import utilities.ReusableMethods;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static stepDefinitions.Hooks.driver;
@@ -15,7 +17,13 @@ public class HomePage extends CommonPage {
 
 
     //@FindBy(xpath = "(//li/a[@href='/account/weekly-order'])[3]")
-   // public WebElement footer_ScheduledDelivery;
+    // public WebElement footer_ScheduledDelivery;
+//    @FindBy(css = "div>.ant-select-selection-search")
+    @FindBy(css = "span>.ant-avatar.ant-avatar-sm.ant-avatar-circle.ant-avatar-image.css-15rg2km")
+
+    public WebElement languages;
+    @FindBy(css = "div>.ant-avatar.ant-avatar-sm.ant-avatar-circle.ant-avatar-image.css-15rg2km>img")
+    public List<WebElement> languagesList;
 
     public void verifyFooterLinks(String expectedCurrentUrl) {
         try {
