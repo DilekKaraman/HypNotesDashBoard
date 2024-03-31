f67ca8b995860e63448210e465cfed1ed4920c1c
ZaferD <test01012018@gmail.com>
2024-03-31 14:12:46 +0200

    US_005 Scenario and Test Case

--- src/test/java/stepDefinitions/apiStepDefs/LanguagesControlApi_StepDefs.java
+++ src/test/java/stepDefinitions/apiStepDefs/LanguagesControlApi_StepDefs.java
@@ -13,37 +13,67 @@ public class LanguagesControlApi_StepDefs {
     @Given("user verifies English language {string}")
     public void user_verifies_english_language(String string) {
         response = given()
-                .contentType("application/json")
-                .header("Authorization", "Bearer your_access_token")
+                .contentType(ContentType.JSON)
                 .when()
                 .get(string);
 
-        // Yanıtı al
         int statusCode = response.getStatusCode();
-        String responseBody = response.getBody().asString();
-
-        // Yanıtı işleme
         System.out.println("Status Code: " + statusCode);
-        System.out.println("Response Body: " + responseBody);
+
     }
+
     @Then("user verifies Turkish language {string}")
     public void user_verifies_turkish_language(String string) {
+        response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(string);
 
+        int statusCode = response.getStatusCode();
+        System.out.println("Status Code: " + statusCode);
     }
+
     @Then("user verifies German language {string}")
     public void user_verifies_german_language(String string) {
+        response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(string);
 
+        int statusCode = response.getStatusCode();
+        System.out.println("Status Code: " + statusCode);
     }
+
     @Then("user verifies French language {string}")
     public void user_verifies_french_language(String string) {
+        response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(string);
 
+        int statusCode = response.getStatusCode();
+        System.out.println("Status Code: " + statusCode);
     }
+
     @Then("user verifies Russian language {string}")
     public void user_verifies_russian_language(String string) {
+        response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(string);
 
+        int statusCode = response.getStatusCode();
+        System.out.println("Status Code: " + statusCode);
     }
+
     @Then("user verifies Spanish language {string}")
     public void user_verifies_spanish_language(String string) {
+        response = given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(string);
 
+        int statusCode = response.getStatusCode();
+        System.out.println("Status Code: " + statusCode);
     }
 }
